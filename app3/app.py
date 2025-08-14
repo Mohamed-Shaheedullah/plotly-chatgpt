12fe48087da1b16b50792c960cde73547aed5d6a
Mohamed Shaheedullah <conifer567@outlook.com>
2025-08-14 09:04:02 +0100

    added comments to app3

--- app3/app.py
+++ app3/app.py
@@ -17,7 +17,7 @@ app.layout = html.Div([
 
     # Multi-select country dropdown
     dcc.Dropdown(
-        id="country-dropdown",
+        id="country-dropdown",  # see line 44
         options=[{"label": c, "value": c} for c in countries],
         value=["Canada", "United States"],  # Default
         multi=True,
@@ -26,7 +26,7 @@ app.layout = html.Div([
 
     # Year range slider
     dcc.RangeSlider(
-        id="year-slider",
+        id="year-slider", # see line 45
         min=df["year"].min(),
         max=df["year"].max(),
         step=5,  # Gapminder dataset has 5-year intervals
@@ -34,17 +34,17 @@ app.layout = html.Div([
         value=[df["year"].min(), df["year"].max()]  # Default full range
     ),
 
-    # Chart
+    # Chart  see line 43
     dcc.Graph(id="lifeexp-chart")
 ])
 
 # Callback for updating chart based on both inputs
 @app.callback(
-    Output("lifeexp-chart", "figure"),
-    [Input("country-dropdown", "value"),
-     Input("year-slider", "value")]
+    Output("lifeexp-chart", "figure"),  # corresponds to chart 
+    [Input("country-dropdown", "value"),  # two inputs correspond to input forms, 1. dropdown
+     Input("year-slider", "value")]       # 2. corresponds to year range slider
 )
-def update_chart(selected_countries, selected_years):
+def update_chart(selected_countries, selected_years):  # two parameters correspond to input forms lines 44, 45
     start_year, end_year = selected_years
     filtered_df = df[
         (df["country"].isin(selected_countries)) &
